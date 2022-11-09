2225916112992f36bb7385859dea41487f3f4cb4
joySarkar8 <sarkarjoy498@gmail.com>
2022-11-09 21:30:45 +0600

    review component added

--- src/Routes/MainRouter.js
+++ src/Routes/MainRouter.js
@@ -5,7 +5,8 @@ import Blog from "../Pages/Blog/Blog";
 import Home from "../Pages/Home/Home/Home";
 import Login from "../Pages/Login/Login";
 import Register from "../Pages/Register/Register";
-import ServiceDetailPage from "../ServiceDetailPage/ServiceDetailPage/ServiceDetailPage";
+import ServicePageDetail from "../ServiceDetailPage/ServicePageDetail/ServicePageDetail";
+
 
 
 const router = createBrowserRouter([
@@ -41,7 +42,7 @@ const router = createBrowserRouter([
             {
                 path: `/services/:id`,
                 loader: ({params}) => fetch(`http://localhost:5000/services/${params.id}`),
-                element: <ServiceDetailPage></ServiceDetailPage>,
+                element: <ServicePageDetail></ServicePageDetail>,
             },
         ]
     }
---
+++ src/ServiceDetailPage/AddReview/AddReview.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import { useLoaderData } from 'react-router-dom';
+
+const AddReview = () => {
+    const { data } = useLoaderData();
+    const { _id, service_name, price, image_url, details } = data;
+    // console.log(data);
+
+    const handlePlaceOrder = event => {
+        event.preventDefault();
+        const form = event.target;
+        const name = form.name.value;
+        const title = form.title.value;
+        const message = form.message.value;
+
+        
+        const review = {
+            service: _id,
+            titleName: title,
+            customerName: name,
+            message,
+        }
+        
+        fetch('http://localhost:5000/reviews', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(review)
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                if (data.data.acknowledged) {
+                    alert('Review placed successfully')
+                    form.reset();
+                }
+            })
+            .catch(er => console.error(er));
+    }
+
+    
+    return (
+        <div className='container text-white p-4 mt-5' style={{ background: '#03031824' }}>
+            <h2 className='mb-4'>Add Review</h2>
+
+            <form onSubmit={handlePlaceOrder}>
+                <div className='row mb-3'>
+                    <div className='col-lg-6 mb-3'>
+                        <input name='name' type="text" placeholder="Your Name" className="input input-bordered w-100 p-2" />
+                    </div>
+                    <div className='col-lg-6'>
+                        <input name='title' type="text" placeholder="Title" className="p-2 input input-bordered w-100" />
+                    </div>
+                </div>
+                <textarea style={{ height: '140px' }} name='message' className="w-100 mb-3 p-3" placeholder="Your Message" required></textarea>
+
+                <input className='btn btn-success' type='submit' value="Post Review"></input>
+            </form>
+
+        </div>
+    );
+};
+
+export default AddReview;
